695f54877616c64cb8d5d9959d41d590024a0157
Niba-nazar <114569256+Niba-nazar@users.noreply.github.com>
2025-11-25 19:51:59 +0530

    chore:update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  // ⬇⬇ THIS is required for GitHub Pages
+  output: 'export',
+
+  // helpful but optional:
+  // images: { unoptimized: true },
+  // trailingSlash: true,
 };
 
 export default withMDX(config);
+
